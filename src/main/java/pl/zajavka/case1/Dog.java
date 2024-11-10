aa74a3da2fff99d1a935485e2d3d375e7b141d2d
Asia Zielinska <joannakabatek@gmail.com>
2024-11-10 19:14:03 +0100

    Modification 3

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,7 +1,10 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
+    private String name;
 
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
     @Override
     public String whatIsYourName() {
         return "My name is Dogyg changed";
